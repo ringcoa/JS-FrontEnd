a0a435f517638b196787f5d68c28aac88afa8ce2
문한주(163229) <luvet@shinsegae.com>
2019-11-05 23:22:14 +0900

    [2019-11-05] 템플릿엔진 코드리뷰

--- 1week/index.js
+++ 1week/index.js
@@ -1,6 +1,27 @@
+// TODO 서브루틴이 전역함수로 선언되어 있습니다 - 모듈패턴으로 리팩토링 해보세요
+const test = (function(...args) {
+    let _privateField;
+    let publicField;
+
+    const _privateMethod = function() {
+
+    }
+    const publicMethod = function() {
+
+    }
+
+    return {
+        "publicMethod": publicMethod,
+        "publicField": publicField
+    }
+}('1', '2', '3'))
+
+// TODO 모듈 내부에서 재사용할 DOM 참조를 미리 캐싱 해두면 좋습니다 - 잘 하셨어요
 var app = document.getElementById('app')
 var loading = document.querySelector('._4emnV')
+// TODO 모듈링시 API 주소값은 모듈 외부에서 파라미터로 전달받도록 해주세요 
 var dataJson = 'https://my-json-server.typicode.com/it-crafts/mockapi/timeline'
+// TODO 상태값을 사용할 경우, 모듈링 및 캡슐화하여 타 모듈에서 접근하여 오염되지 않도록 해주세요
 var imgData =[];
 var viewImgHtml = ``;
 
@@ -8,9 +29,12 @@ var viewImgHtml = ``;
      //console.log(response)
      return response.json();
    }).then(function(myJson){
+        // FIXME 불필요한 디스트럭쳐링 같습니다 - 본 케이스엔 이슈 없어보이니, imgData = myJson 이렇게 해주세요
        imgData.push(...myJson)
     }).then(init)
 
+// FIXME 필드명 src1, src2, src3이 하드코딩 되어 있습니다 - 아무 필드명이나 대응할 수 있도록 수정 해주세요 
+// FIXME 템플릿이 하드코딩 되어 있습니다 - 임의의 템플릿을 대응할 수 있도록 구조변경 해주세요
 function getData(){
     imgData.forEach(element => {
         viewImgHtml = viewImgHtml+ `
--- test.js
+++ test.js
@@ -1,8 +1,6 @@
-//XXX 확인요망 코멘트 테스트!!
 const main = (...args) => {
     args.forEach(e => {
         console.log(e);
     });
 }
-// FIXME 픽스미 코맨트 테스트
 main('test1', 'test2', 'test3');
