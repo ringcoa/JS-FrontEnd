fd5d080513e6f646f3064337d00ef549f47b1fba
ringcoa <gkakdlv@gmail.com>
2019-11-13 23:08:08 +0900

    2week

--- 1week/index.js
+++ 1week/index.js
@@ -1,93 +1,88 @@
+/*
+1. 템플릿엔진 모듈
+2. 컴포넌트
+
+
+*/
+const timelineComponent =`<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
+<div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{src1}}" style="object-fit: cover;"></div>
+<div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+<div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{  src2 }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+<div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div>
+<div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+<div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{ src3  }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+<div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div></div>`
+
 // TODO 서브루틴이 전역함수로 선언되어 있습니다 - 모듈패턴으로 리팩토링 해보세요
-const test = (function(...args) {
-    let _privateField;
-    let publicField;
+const templateEngine = (function(jsonData , timeline) {
+    var _app = document.getElementById('app')
+    var _loading = document.querySelector('._4emnV')
+    var _viewTab = document.querySelector('._9VEo1')
+    var _imgData =[];
+    var _viewImgHtml = timeline;
+  
+    
 
-    const _privateMethod = function() {
+    (async function() {
+        const res = await fetch(jsonData);
+        _imgData = (await res.json());
+       
+        
+    }());
 
+    const getData = ()=>{
+        console.log('gg')
     }
-    const publicMethod = function() {
 
+    return{
+        testing : function(){
+            console.log(_viewImgHtml)
+        }   
     }
 
-    return {
-        "publicMethod": publicMethod,
-        "publicField": publicField
-    }
-}('1', '2', '3'))
 
-// TODO 모듈 내부에서 재사용할 DOM 참조를 미리 캐싱 해두면 좋습니다 - 잘 하셨어요
-var app = document.getElementById('app')
-var loading = document.querySelector('._4emnV')
-// TODO 모듈링시 API 주소값은 모듈 외부에서 파라미터로 전달받도록 해주세요 
-var dataJson = 'https://my-json-server.typicode.com/it-crafts/mockapi/timeline'
+    // function getData(){
+    //     imgData.forEach(element => {
+    //         viewImgHtml = viewImgHtml+
+    //     });
+    // }
+
+    
+}('https://my-json-server.typicode.com/it-crafts/mockapi/timeline',timelineComponent))
+
+//html += template.replace(/{{ *(\w+) *}}/g, (m, key) => data['key'] || '');
+
+
+// const 타임라인컴포넌트 = (function(...args) {
+//     let _privateField;
+//     let publicField;
+
+//     const _privateMethod = function() {
+
+//     }
+//     const publicMethod = function() {
+
+//     }
+
+//     return {
+//         "publicMethod": publicMethod,
+//         "publicField": publicField 
+//     }
+// }('1', '2', '3'))
+
+
+
 // TODO 상태값을 사용할 경우, 모듈링 및 캡슐화하여 타 모듈에서 접근하여 오염되지 않도록 해주세요
-var imgData =[];
-var viewImgHtml = ``;
 
- fetch(dataJson).then(function(response){
-     //console.log(response)
-     return response.json();
-   }).then(function(myJson){
-        // FIXME 불필요한 디스트럭쳐링 같습니다 - 본 케이스엔 이슈 없어보이니, imgData = myJson 이렇게 해주세요
-       imgData.push(...myJson)
-    }).then(init)
+
 
 // FIXME 필드명 src1, src2, src3이 하드코딩 되어 있습니다 - 아무 필드명이나 대응할 수 있도록 수정 해주세요 
 // FIXME 템플릿이 하드코딩 되어 있습니다 - 임의의 템플릿을 대응할 수 있도록 구조변경 해주세요
-function getData(){
-    imgData.forEach(element => {
-        viewImgHtml = viewImgHtml+ `
-        <div class="Nnq7C weEfm">
-            <div class="v1Nh3 kIKUG _bz0w">
-                <a href="javascript:;">
-                <div class="eLAPa">
-                    <div class="KL4Bh">
-                        <img class="FFVAD" decoding="auto" src="${element['src1']}">
-                    </div>  
-                    <div class="_9AhH0">
-                    </div>
-                </div>
-                <div class="u7YqG">
-                    <span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7">
-                    </span>
-                </div>
-                </a>
-            </div>
-            <div class="v1Nh3 kIKUG _bz0w">
-                <a href="javascript:;">
-                <div class="eLAPa">
-                    <div class="KL4Bh">
-                        <img class="FFVAD" decoding="auto"  src="${element['src2']}">
-                    </div>
-                    <div class="_9AhH0">
-                    </div>
-                </div>
-                <div class="u7YqG">
-                    <span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7">
-                    </span>
-                </div>
-                </a>
-            </div>
-            <div class="v1Nh3 kIKUG _bz0w">
-                <a href="javascript:;">
-                <div class="eLAPa">
-                    <div class="KL4Bh">
-                        <img class="FFVAD" decoding="auto" src="${element['src3']}">
-                    </div>
-                    <div class="_9AhH0">
-                    </div>
-                </div>
-                <div class="u7YqG">
-                    <span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7">
-                    </span>
-                </div>
-                </a>
-            </div>
-        </div>
-        `
-    });
+view = {
+    timeline:[]
 }
+
+
 
 function viewImg(){
     loading.style.display="none"
---
+++ 1week/script.js
@@ -0,0 +1,115 @@
+const _loading = document.querySelector('._4emnV');
+
+const timeline = (function() {
+    const template = `<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
+        <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{src1}}" style="object-fit: cover;"></div>
+        <div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+        <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{  src2 }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+        <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div>
+        <div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+        <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{ src3  }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+        <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div></div>`;
+    const url = 'https://my-json-server.typicode.com/it-crafts/mockapi/timeline/';
+    const selector = '#app';
+    let page = 1;
+    const child = document.querySelector('.Nnq7C')
+    
+
+
+    const render = async function() {
+        // model
+       
+        const { data } = await axios(url + page++);
+        
+        console.log(data)
+    
+        
+        const list = data.data.map(l => l.reduce((o, v, i) => (o[`src${i+1}`] = v, o), {}));
+        
+
+        // controller
+        let html = '';
+        list.forEach(data => {
+            //console.log(data);
+            html += template.replace(/{{ *(\w+) *}}/g, (m, key) => data[key] || '');
+        })
+
+        // view
+        const app = document.querySelector(selector); 
+        app.innerHTML += html;
+
+
+       
+    }
+
+    const del = function(){ 
+        console.log(page)
+        page = 1;
+        console.log(page)
+
+        app.removeChild(child)
+      
+    }
+
+    return {
+        render: render,
+        del : del
+    }
+}());
+
+const test = function(test) {
+    let _test = test;
+    console.log(_test);
+}
+test('test'); // test
+test(); // undefined
+
+function Constructor() {
+    let _test;
+    this.test = function(test) {
+        _test = _test || test;
+        console.log(_test);
+    }
+};
+const TEST = new Constructor();
+TEST.test('TEST'); // TEST
+TEST.test(); // TEST
+
+const root = (async function() {
+    
+    await timeline.render();
+    // await timeline.render();
+    // await timeline.render();
+    // await timeline.render();
+    // await timeline.render();
+    // await timeline.render();
+}())
+
+document.querySelectorAll('.fx7hk > a').forEach(tabButton => {
+    tabButton.addEventListener('click', async function(e) {
+        // TODO 버튼 누를 때는 1페이지로 새로 요청을 해야 함
+        if('' === _loading.style.display) {
+            page = 1;
+        }
+        _loading.style.display = '';
+        await timeline.render();
+        _loading.style.display = 'none';
+    });
+});
+
+window.addEventListener('scroll', async function(e) {
+    // TODO 화면의 적절한 위치까지 갔을 때만 Ajax 요청
+      if(pageYOffset > app.scrollHeight) { // TODO 100 대신 페이지의 거의 마지막에 닿은 Y좌표를 동적으로 가져온다
+         timeline.render(); //순서대로 안되고 한번에여러게의 데이터를 받아오네...ㅜㅜ
+         clearTimeout(timeline.render)
+        
+      }
+    if('' === _loading.style.display) {
+        return;
+    }
+    _loading.style.display = '';
+    // TODO info API에서 totalPage 받아와서, 거기까지만 요청하게 수정
+
+    await timeline.render();
+    _loading.style.display = 'none';
+});
